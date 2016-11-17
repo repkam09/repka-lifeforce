713fbdc59e594ad0f5e14c8c386b0f0efe7b99ab
Mark Repka <mark@repkam09.com>
2016-11-17 12:39:45 -0500

    Add election results endpoint

--- plugins/election.js
+++ plugins/election.js
@@ -1,7 +1,13 @@
 const log = require("../utils/logger");
+const election_results = require("../static/election_results.json");
 
 function addHandlers(server) {
-
+    server.get("/api/election/full", (req, res, next) => {
+        // This used to be dynamic and updated from CNN website, but since election
+        // is now over, use a static file to give results. Faster and more reliable since 
+        // the information is no longer changing.
+        res.send(200, election_results);
+    });
 }
 
 /**
@@ -9,7 +15,7 @@ function addHandlers(server) {
  * You must have an enabled, name, and start property defined
  */
 module.exports = {
-    enabled: false,
+    enabled: true,
     name: "election",
     start: (server) => {
         addHandlers(server);
