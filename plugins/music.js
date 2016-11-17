090d4690c0e0f4fad59adceec694653425dc8fa8
Mark Repka <mark@repkam09.com>
2016-11-17 12:52:23 -0500

    Add music now playing apis

--- plugins/music.js
+++ plugins/music.js
@@ -1,7 +1,25 @@
 const log = require("../utils/logger");
+const request = require('request');
+const config = require("../config.json");
 
 function addHandlers(server) {
+    server.get("/api/music/now/:name", (req, res, next) => {
+        if (req.params.name) {
+            var url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=" + req.params.name + "&api_key=" + config.lastfmapi + "&format=json";
+            request.get(url).pipe(res);
+        } else {
+            res.send(400);
+        }
+    });
 
+    server.get("/api/music/recent/:name", (req, res, next) => {
+        if (req.params.name) {
+            var url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + req.params.name + "&api_key=" + config.lastfmapi + "&format=json";
+            request.get(url).pipe(res);
+        } else {
+            res.send(400);
+        }
+    });
 }
 
 /**
@@ -9,7 +27,7 @@ function addHandlers(server) {
  * You must have an enabled, name, and start property defined
  */
 module.exports = {
-    enabled: false,
+    enabled: true,
     name: "music",
     start: (server) => {
         addHandlers(server);
