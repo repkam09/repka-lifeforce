8ebabe1869d405089e05ca17d46b45c87f10ca70
Mark Repka <mark@repkam09.com>
2016-11-17 13:00:10 -0500

    Add some quick utility apis

--- plugins/utils.js
+++ plugins/utils.js
@@ -1,7 +1,7 @@
 const log = require("../utils/logger");
+const request = require('request');
 
 function addHandlers(server) {
-    log.debug("Adding handler for about api get request");
     server.get("/api/about", (req, res, next) => {
         var apis = [];
         var keys = Object.keys(server.router.mounts);
@@ -12,7 +12,34 @@ function addHandlers(server) {
 
         res.send(200, apis);
     });
+
+
+    server.get("/api/imgur/:url", (req, res, next) => {
+        if (req.params.url) {
+            request.get("http://i.imgur.com/" + req.params.url + ".jpg").pipe(res);
+        }
+    });
+
+    server.post("/api/b64", (req, res, next) => {
+        if (req.body) {
+            var b64 = new Buffer(req.body).toString('base64');
+            res.send(200, b64);
+        } else {
+            res.send(400, "Bad Request");
+        }
+    });
+
+    server.post("/api/d64", (req, res, next) => {
+        if (req.body) {
+            var d64 = new Buffer(req.body, 'base64').toString('ascii')
+            res.send(200, d64);
+        } else {
+            res.send(400, "Bad Request");
+        }
+    });
+
 }
+
 
 /**
  * This set of properties defines this as a plugin
