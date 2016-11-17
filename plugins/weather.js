22aaaf02c6df4192976ba302589a19ef3a52fbf8
Mark Repka <mark@repkam09.com>
2016-11-17 12:52:57 -0500

    Add current weather apis

--- plugins/weather.js
+++ plugins/weather.js
@@ -1,7 +1,25 @@
 const log = require("../utils/logger");
+const request = require('request');
+const config = require("../config.json");
 
 function addHandlers(server) {
+    server.get("/api/weather/current/zip/:zip", (req, res, next) => {
+        if (req.params.zip) {
+            let url = "http://api.openweathermap.org/data/2.5/weather?zip=" + req.params.zip + " &appid=" + config.weatherapikey;
+            request.get(url).pipe(res);
+        } else {
+            res.send(400);
+        }
+    });
 
+    server.get("/api/weather/current/name/:name", (req, res, next) => {
+        if (req.params.name) {
+            let url = "http://api.openweathermap.org/data/2.5/weather?q=" + req.params.name + " &appid=" + config.weatherapikey;
+            request.get(url).pipe(res);
+        } else {
+            res.send(400);
+        }
+    });
 }
 
 /**
@@ -9,7 +27,7 @@ function addHandlers(server) {
  * You must have an enabled, name, and start property defined
  */
 module.exports = {
-    enabled: false,
+    enabled: true,
     name: "weather",
     start: (server) => {
         addHandlers(server);
