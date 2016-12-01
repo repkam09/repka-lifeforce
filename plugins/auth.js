8592af7c9b8871a0ea54779b14f49f41c17034bc
Mark Repka <mark@repkam09.com>
2016-12-01 16:47:31 -0500

    Start some auth stuff

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,17 +1,45 @@
 const log = require("../utils/logger");
 const restify = require('restify');
+const config = require("../config.json");
 
 var authlist = [
-    "getapilifeforceheartbeatappname110"
 ]
 
 function addHandlers(server) {
     server.use(function authenticate(req, res, next) {
         if (authlist.indexOf(req._currentRoute) != -1) {
-            log.info("Authentication Required: " + req.url);
-            //return next(new restify.NotAuthorizedError());
+            log.verbose("Authentication Required: " + req.url);
+            // Get the username and password that were passed in with the auth request
+
+            let authinfo = req.authorization;
+            if (authinfo.scheme === "Basic") {
+                let authlist = config.authlist;
+
+                // Get the username and password that were supplied
+                let suppliedusername = authinfo.basic.username;
+                let suppliedpassword = authinfo.basic.password;
+
+                // Check if the supplied username exists in the known users list
+                if (authlist[suppliedusername]) {
+                    let correctpass = authlist[suppliedusername];
+                    log.verbose("Authentication attempt using username " + suppliedusername);
+                    if (suppliedpassword === correctpass) {
+                        // Forward this request on, the user is allowed
+                        return next();
+                    } else {
+                        // The username exists, but the password is incorrect
+                        return next(new restify.NotAuthorizedError());
+                    }
+                } else {
+                    // The username specified does not exist in the list
+                    return next(new restify.NotAuthorizedError());
+                }
+            } else {
+                // The wrong auth scheme was supplied
+                return next(new restify.NotAuthorizedError());
+            }
         } else {
-            log.info("No authentication required for " + req.url);
+            log.verbose("No authentication required for " + req.url);
         }
         return next();
     });
