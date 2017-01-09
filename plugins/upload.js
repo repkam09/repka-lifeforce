d35045db8c8dfb05c2a354180a51ec1029b9ffc0
Mark Repka <mark@repkam09.com>
2017-01-09 13:23:26 -0500

    Add simple image upload endpoint

--- plugins/upload.js
+++ plugins/upload.js
@@ -3,28 +3,51 @@ const config = require("../config.json");
 const fs = require("fs");
 
 function addHandlers(server) {
-server.post("/api/files/upload", (req, res, next) => {
-    if (req.files) {
-        var data = req.files.filedata;
-        var path = data.path;
-        var newpath = config.logpathhidden + "uploads/" + data.name;
-        fs.rename(path, newpath, (err) => {
-	    if (err) {
-		log.error("Error moving file from " + path + " to " + newpath);
-	    } else {
-            	log.verbose("Moved file from " + path + " to " + newpath);
-	    }
-        });
+    server.post("/api/files/upload", (req, res, next) => {
+        if (req.files) {
+            var data = req.files.filedata;
+            var path = data.path;
+            var newpath = config.logpathhidden + "uploads/" + data.name;
+            fs.rename(path, newpath, (err) => {
+                if (err) {
+                    log.error("Error moving file from " + path + " to " + newpath);
+                } else {
+                    log.verbose("Moved file from " + path + " to " + newpath);
+                }
+            });
 
-        log.info("Got file: " + data.name);
-        log.verbose("Hash: " + data.hash);
-        res.send(200, "https://files.repkam09.com/" + data.hash);
-    } else {
-        res.send(200, { filename: false });
-    }
+            log.info("Got file: " + data.name);
+            log.verbose("Hash: " + data.hash);
+            res.send(200, "https://files.repkam09.com/" + data.hash);
+        } else {
+            res.send(200, { filename: false });
+        }
 
-    next();
-});
+        next();
+    });
+
+    server.post("/api/images/upload", (req, res, next) => {
+        if (req.files) {
+            var data = req.files.filedata;
+            var path = data.path;
+            var newpath = config.publicpath + data.hash;
+            fs.rename(path, newpath, (err) => {
+                if (err) {
+                    log.error("Error moving file from " + path + " to " + newpath);
+                } else {
+                    log.verbose("Moved file from " + path + " to " + newpath);
+                }
+            });
+
+            log.info("Got file: " + data.name);
+            log.verbose("Hash: " + data.hash);
+            res.send(200, "https://repkam09.com/i/" + data.hash);
+        } else {
+            res.send(200, { filename: false });
+        }
+
+        next();
+    });
 }
 
 /**
