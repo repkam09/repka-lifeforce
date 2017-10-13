cc8610ba0aca63fc1a646f2885d9a19c4744844c
repkam09 <mark@repkam09.com>
2017-10-13 14:38:14 -0400

    Minor fix to file uploads

--- plugins/fileupload.js
+++ plugins/fileupload.js
@@ -1,5 +1,5 @@
 const LifeforcePlugin = require("../utils/LifeforcePlugin.js");
-
+const fs = require("fs");
 
 
 class FileUploads extends LifeforcePlugin {
@@ -24,18 +24,20 @@ function handleFileUpload(req, res, next) {
     if (req.files) {
         var data = req.files.filedata;
         var path = data.path;
-        var newpath = config.logpathhidden + "uploads/" + data.name;
+        var newpath = this.config.logpathhidden + "uploads/" + data.name;
+	console.log("Current Path:" + path);
+	console.log("New Path:" + newpath);
         fs.rename(path, newpath, (err) => {
             if (err) {
-                log.error("Error moving file from " + path + " to " + newpath);
+                this.log.error("Error moving file from " + path + " to " + newpath);
             } else {
-                log.verbose("Moved file from " + path + " to " + newpath);
+                this.log.verbose("Moved file from " + path + " to " + newpath);
             }
         });
 
-        log.info("Got file: " + data.name);
-        log.verbose("Hash: " + data.hash);
-        res.send(200, "https://files.repkam09.com/" + data.hash);
+        this.log.info("Got file: " + data.name);
+        this.log.verbose("Hash: " + data.hash);
+        res.send(200, {url: "https://repkam09.com/dl/uploads/" + data.name});
     } else {
         res.send(200, { filename: false });
     }
