159bb27d76343b3012c5667496107bb527b0ef80
repkam09 <mark@repkam09.com>
2017-10-17 13:19:13 -0400

    Add test torque endpoint

--- plugins/torque.js
+++ plugins/torque.js
@@ -9,12 +9,18 @@ class Torque extends LifeforcePlugin {
                 path: "/api/torque/log",
                 type: "post",
                 handler: handleTorquePost
-            }
+            },
+	    {
+		path: "/api/torque/log",
+		type: "get",
+		handler: handleTorqueGet
+	    }
         ];
     }
 }
 
 function handleTorquePost(req, res, next) {
+    this.log.info("Got a torque post request");
     if (req.body) {
         this.log.special(req.body);
     }
@@ -23,6 +29,11 @@ function handleTorquePost(req, res, next) {
         this.log.special(req.params);
     }
 
+    res.send(200, "OK!");
+}
+
+function handleTorqueGet(req, res, next) {
+    this.log.info("Got a torque get request");
     res.send(200, "OK!");
 }
 
