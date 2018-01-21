420a3dda7c66c2c788ccc8bfb715dc3a29ba0b80
Mark Repka <mark@repkam09.com>
2018-01-21 16:48:44 -0500

    Add better mimetype mapping for repcast

--- backend_helper/repcast.php
+++ backend_helper/repcast.php
@@ -21,6 +21,84 @@ class RepcastResponse {
 	public $log;
 }
 
+// override the old mimetype thing
+function _mime_content_type($filename) {
+
+	$mime_types = array(
+
+		'txt' => 'text/plain',
+		'htm' => 'text/html',
+		'html' => 'text/html',
+		'php' => 'text/html',
+		'css' => 'text/css',
+		'js' => 'application/javascript',
+		'json' => 'application/json',
+		'xml' => 'application/xml',
+		'swf' => 'application/x-shockwave-flash',
+		'flv' => 'video/x-flv',
+
+		// images
+		'png' => 'image/png',
+		'jpe' => 'image/jpeg',
+		'jpeg' => 'image/jpeg',
+		'jpg' => 'image/jpeg',
+		'gif' => 'image/gif',
+		'bmp' => 'image/bmp',
+		'ico' => 'image/vnd.microsoft.icon',
+		'tiff' => 'image/tiff',
+		'tif' => 'image/tiff',
+		'svg' => 'image/svg+xml',
+		'svgz' => 'image/svg+xml',
+
+		// archives
+		'zip' => 'application/zip',
+		'rar' => 'application/x-rar-compressed',
+		'exe' => 'application/x-msdownload',
+		'msi' => 'application/x-msdownload',
+		'cab' => 'application/vnd.ms-cab-compressed',
+
+		// audio/video
+		'mp3' => 'audio/mpeg',
+		'qt' => 'video/quicktime',
+		'mov' => 'video/quicktime',
+		'mp4' => 'video/mp4',
+		'mkv' => 'video/x-matroska',
+		'avi' => 'video/avi',
+
+		// adobe
+		'pdf' => 'application/pdf',
+		'psd' => 'image/vnd.adobe.photoshop',
+		'ai' => 'application/postscript',
+		'eps' => 'application/postscript',
+		'ps' => 'application/postscript',
+
+		// ms office
+		'doc' => 'application/msword',
+		'rtf' => 'application/rtf',
+		'xls' => 'application/vnd.ms-excel',
+		'ppt' => 'application/vnd.ms-powerpoint',
+
+		// open office
+		'odt' => 'application/vnd.oasis.opendocument.text',
+		'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+	);
+
+	$ext = strtolower(array_pop(explode('.',$filename)));
+	if (array_key_exists($ext, $mime_types)) {
+		return $mime_types[$ext];
+	}
+	elseif (function_exists('finfo_open')) {
+		$finfo = finfo_open(FILEINFO_MIME);
+		$mimetype = finfo_file($finfo, $filename);
+		finfo_close($finfo);
+		return $mimetype;
+	}
+	else {
+		return 'application/octet-stream';
+	}
+}
+
+
 // Create the final response object
 $repcastresponse = new RepcastResponse();
 $repcastfiles = array();
@@ -64,18 +142,18 @@ foreach ($files as $filedir) {
 		//array_push($repcastresponse->log, $filedir . " is a file");
 
 		// Get some file information like extention and mimetype
-		$mimetype = mime_content_type($fullpath . $filedir);
+		$mimetype = _mime_content_type($fullpath . $filedir);
 		$fileinfo = pathinfo($fullpath . $filedir);
 		//array_push($repcastresponse->log, $filedir . " is of type " . $mimetype);
 
 		$file = new RepcastFile();
 		$file->name = $filedir;
-		$file->path = "http://192.168.0.3/UI/data/shares/repcast/". $subdir . "/" . $filedir;
+		$file->path = "http://192.168.0.3/UI/data/shares/repcast/". $subdir . "/" . rawurlencode($filedir);
 		$file->type = "file";
 		$file->size = 0;
 		$file->original = $filedir;
 		$file->filetype = $fileinfo['extension'];
-		$file->mimetype = "video/" .$fileinfo['extension'];
+		$file->mimetype = $mimetype;
 	}
 
 	array_push($repcastfiles, $file);
