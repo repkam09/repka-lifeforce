5a0f409e40a4a8c1abc32c94f35cd61fb52c3b5f
Mark Repka <mark@repkam09.com>
2019-06-28 15:05:31 -0400

    Minor package cleanup

--- plugins/meta.js
+++ plugins/meta.js
@@ -4,75 +4,81 @@ const LifeforcePlugin = require("../utils/LifeforcePlugin.js");
 const serverhostname = "https://api.repkam09.com";
 
 class MetaEndpoints extends LifeforcePlugin {
-    constructor(restifyserver, logger, name) {
-        super(restifyserver, logger, name);
-        this.apiMap = [
-            {
-                path: "/api/about",
-                type: "get",
-                handler: handleAboutApi
-            },
-            {
-                path: "/",
-                type: "get",
-                handler: handleAboutApi
-            },
-            {
-                path: "/api/gettest",
-                type: "get",
-                handler: handleGetTest
-            },
-            {
-                path: "/api/posttest",
-                type: "post",
-                handler: handlePostTest
-            },
-            {
-                path: "/api/getip",
-                type: "get",
-                handler: handleGetIp
-            }
-
-        ];
-    }
+  constructor(restifyserver, logger, name) {
+    super(restifyserver, logger, name);
+    this.apiMap = [
+      {
+        path: "/api/about",
+        type: "get",
+        handler: handleAboutApi
+      },
+      {
+        path: "/",
+        type: "get",
+        handler: handleAboutApi
+      },
+      {
+        path: "/api/gettest",
+        type: "get",
+        handler: handleGetTest
+      },
+      {
+        path: "/api/posttest",
+        type: "post",
+        handler: handlePostTest
+      },
+      {
+        path: "/api/getip",
+        type: "get",
+        handler: handleGetIp
+      }
+    ];
+  }
 }
 
 function handleGetIp(req, res, next) {
-    var clientip = "unknown";
-    if (req.headers['x-forwarded-for']) {
-        clientip = req.headers['x-forwarded-for'];
-    } else if (req.connection.remoteAddress) {
-        clientip = req.connection.remoteAddress;
-    }
-    res.send(200, { ip: clientip });
-    next();
+  var clientip = "unknown";
+  if (req.headers["x-forwarded-for"]) {
+    clientip = req.headers["x-forwarded-for"];
+  } else if (req.connection.remoteAddress) {
+    clientip = req.connection.remoteAddress;
+  }
+  res.send(200, { ip: clientip });
+  next();
 }
 
 function handleAboutApi(req, res, next) {
-    var apis = [];
-    var keys = Object.keys(this.restifyserver.router.mounts);
-    keys.forEach((key) => {
-        var current = this.restifyserver.router.mounts[key];
-        apis.push({ path: serverhostname + current.spec.path, method: current.method, plugin: current.plugin });
+  var apis = [];
+  var keys = Object.keys(this.restifyserver.router.mounts);
+  keys.forEach(key => {
+    var current = this.restifyserver.router.mounts[key];
+    apis.push({
+      path: serverhostname + current.spec.path,
+      method: current.method,
+      plugin: current.plugin
     });
-    res.send(200, apis);
+  });
+  res.send(200, apis);
 }
 
 function handleGetTest(req, res, next) {
-    res.send(200, "You have made a GET request! OK!");
+  res.send(200, "You have made a GET request! OK!");
 }
 
 function handlePostTest(req, res, next) {
-    if (req.body) {
-        res.send(200, req.body);
+  if (req.body) {
+    res.send(200, req.body);
+  } else {
+    if (req.params) {
+      res.send(200, req.params);
     } else {
-        if (req.params) {
-            res.send(200, req.params);
-        } else {
-            res.send(200, "You dont seem to have POSTed anything. But we got the request.");
-        }
+      res.send(
+        200,
+        "You dont seem to have POSTed anything. But we got the request."
+      );
     }
-    next();
+  }
+  next();
 }
 
 module.exports = MetaEndpoints;
--- plugins/repcast.js
+++ plugins/repcast.js
@@ -1,215 +1,173 @@
 const LifeforcePlugin = require("../utils/LifeforcePlugin.js");
 const fs = require("fs");
 const path = require("path");
-const trans = require('transmission');
+const trans = require("transmission");
 
 let pathfix = "";
 
 class RepCast extends LifeforcePlugin {
-    constructor(restifyserver, logger, name) {
-        super(restifyserver, logger, name);
-        this.apiMap = [
-            {
-                path: "/repcast/dirget/:filepath",
-                type: "get",
-                handler: handleRepcastDirGet
-            },
-            {
-                path: "/repcast/fileget/:type",
-                type: "get",
-                handler: handleRepcastFileTypeGet
-            },
-            {
-                path: "/repcast/torsearch/:search",
-                type: "get",
-                handler: handleRepcastTorSearch
-            },
-            {
-                path: "/repcast/toradd/:magnet",
-                type: "get",
-                handler: handleRepcastTorAdd
-            },
-            {
-                path: "/repcast/dialogflow",
-                type: "post",
-                handler: handleDialogFlow
-            },
-            {
-                path: "/repcast/dialogflow",
-                type: "get",
-                handler: handleDialogFlow
-            }
-        ];
+  constructor(restifyserver, logger, name) {
+    super(restifyserver, logger, name);
+    this.apiMap = [
+      {
+        path: "/repcast/dirget/:filepath",
+        type: "get",
+        handler: handleRepcastDirGet
+      },
+      {
+        path: "/repcast/fileget/:type",
+        type: "get",
+        handler: handleRepcastFileTypeGet
+      },
+      {
+        path: "/repcast/toradd/:magnet",
+        type: "get",
+        handler: handleRepcastTorAdd
+      }
+    ];
 
-        // Grab some specific values from the config
-        this.settings = this.config.torrent;
-        pathfix = this.config.mediamount;
-
-        this.tpb = require("thepiratebay");
-    }
-}
-
-function handleDialogFlow(req, res, next) {
-    var responseData = "OK!";
-    if (req.body) {
-        this.log.special("Body Found: " + typeof req.body + " " + JSON.stringify(req.body));
-        responseData = req.body.result.parameters.video_name;
-    }
-
-    if (req.params) {
-        this.log.special("Params Found: " + typeof req.params + " " + JSON.stringify(req.params));
-    }
-
-    res.send(200, responseData);
+    // Grab some specific values from the config
+    this.settings = this.config.torrent;
+    pathfix = this.config.mediamount;
+  }
 }
 
 function handleRepcastDirGet(req, res, next) {
-    // This call has been disabled as the new repcast is active now
-    res.send(200, {
-        result: [{
-            "name": "Please Update RepCast",
-            "type": "dir",
-            "path": "/",
-            "cast": false,
-            "video": false,
-            "path64": "Lw=="
-        }]
-    });
+  // This call has been disabled as the new repcast is active now
+  res.send(200, {
+    result: [
+      {
+        name: "Please Update RepCast",
+        type: "dir",
+        path: "/",
+        cast: false,
+        video: false,
+        path64: "Lw=="
+      }
+    ]
+  });
 
-    return next();
+  return next();
 }
 
 function handleRepcastFileTypeGet(req, res, next) {
-    var ftype = "." + req.params.type; //new Buffer(req.params.type, 'base64').toString();
-    if (ftype === "") {
-        res.send(200, { result: [] });
-        return next();
-    } else {
-        this.log.verbose("Requested listing for file type " + ftype);
+  var ftype = "." + req.params.type; //new Buffer(req.params.type, 'base64').toString();
+  if (ftype === "") {
+    res.send(200, { result: [] });
+    return next();
+  } else {
+    this.log.verbose("Requested listing for file type " + ftype);
 
-        var list = filelist(pathfix, ftype);
+    var list = filelist(pathfix, ftype);
 
-        // Go through the list checking that the file ends in type
+    // Go through the list checking that the file ends in type
 
-        res.send(200, { result: list });
-        return next();
-    }
-}
-
-function handleRepcastTorSearch(req, res, next) {
-    var searchterm = new Buffer(req.params.search, 'base64').toString();
-    this.log.verbose("torrent serarch for : " + searchterm);
-
-    var test = this.tpb.search(searchterm).then((results) => {
-        this.log.verbose("torrent serarch results: " + JSON.stringify(results));
-        var obj = {};
-        obj.query = searchterm;
-        obj.count = results.length;
-        obj.torrents = results;
-        res.send(200, obj);
-    }).catch((error) => {
-        this.log.error("Got an error for " + searchterm);
-        var obj = {};
-        obj.count = 0;
-        obj.query = "Error Searching TPB";
-        obj.torrents = [];
-        res.send(500, obj);
-    });
+    res.send(200, { result: list });
+    return next();
+  }
 }
 
 function handleRepcastTorAdd(req, res, next) {
-    var magnet = new Buffer(req.params.magnet, 'base64').toString();
-    this.log.verbose("Request on toradd for " + magnet);
+  var magnet = new Buffer(req.params.magnet, "base64").toString();
+  this.log.verbose("Request on toradd for " + magnet);
 
-    var instance = new trans({ port: this.settings.port, host: this.settings.host, username: this.settings.username, password: this.settings.password });
-    instance.addUrl(magnet, {}, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.send(400, err);
-        } else {
-            var id = result.id;
-            res.send(200, { torrentid: id });
-        }
-    });
+  var instance = new trans({
+    port: this.settings.port,
+    host: this.settings.host,
+    username: this.settings.username,
+    password: this.settings.password
+  });
+  instance.addUrl(magnet, {}, function(err, result) {
+    if (err) {
+      console.log(err);
+      res.send(400, err);
+    } else {
+      var id = result.id;
+      res.send(200, { torrentid: id });
+    }
+  });
 }
 
 function filelist(path, ftype) {
-    var list = walk(path);
-    list = list.filter((element) => {
-        if (element.endsWith(ftype)) {
-            return true;
-        }
-    });
+  var list = walk(path);
+  list = list.filter(element => {
+    if (element.endsWith(ftype)) {
+      return true;
+    }
+  });
 
-    list = list.map((element) => {
-        return element.replace(pathfix, "");
-    });
-    return list;
+  list = list.map(element => {
+    return element.replace(pathfix, "");
+  });
+  return list;
 }
 
-var walk = function (dir) {
-    var results = []
-    var list = fs.readdirSync(dir)
-    list.forEach(function (file) {
-        file = dir + '/' + file
-        var stat = fs.statSync(file)
-        if (stat && stat.isDirectory()) results = results.concat(walk(file))
-        else results.push(file)
-    })
-    return results
-}
+var walk = function(dir) {
+  var results = [];
+  var list = fs.readdirSync(dir);
+  list.forEach(function(file) {
+    file = dir + "/" + file;
+    var stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) results = results.concat(walk(file));
+    else results.push(file);
+  });
+  return results;
+};
 
 function dirlist(filepath) {
-    // get the list of files in this directory:
-    var files = fs.readdirSync(filepath);
+  // get the list of files in this directory:
+  var files = fs.readdirSync(filepath);
 
-    files.sort(function (a, b) {
-        return fs.statSync(filepath + b).mtime.getTime() - fs.statSync(filepath + a).mtime.getTime();
-    });
+  files.sort(function(a, b) {
+    return (
+      fs.statSync(filepath + b).mtime.getTime() -
+      fs.statSync(filepath + a).mtime.getTime()
+    );
+  });
 
-    var filelist = [];
+  var filelist = [];
 
-    files.forEach(function (file) {
-        var fixpath = filepath.replace(pathfix, "");
+  files.forEach(function(file) {
+    var fixpath = filepath.replace(pathfix, "");
 
-        var pathb64 = new Buffer(fixpath + file).toString('base64');
+    var pathb64 = new Buffer(fixpath + file).toString("base64");
 
-        var jsonstruct = {
-            name: file,
-            type: "file",
-            path: fixpath + file,
-            cast: false,
-            video: false
-        };
+    var jsonstruct = {
+      name: file,
+      type: "file",
+      path: fixpath + file,
+      cast: false,
+      video: false
+    };
 
-        var stats = fs.statSync(filepath + file);
-        // If something is a directory do some extra operations, and include it
-        if (stats.isDirectory()) {
-            jsonstruct.type = "dir";
-            jsonstruct.path = jsonstruct.path + "/";
-            jsonstruct.path64 = new Buffer(jsonstruct.path).toString('base64');
+    var stats = fs.statSync(filepath + file);
+    // If something is a directory do some extra operations, and include it
+    if (stats.isDirectory()) {
+      jsonstruct.type = "dir";
+      jsonstruct.path = jsonstruct.path + "/";
+      jsonstruct.path64 = new Buffer(jsonstruct.path).toString("base64");
 
-            filelist.push(jsonstruct);
-        } else {
-            // Not a directory. Check if its a castable file type (mp4)
-            var pathext = path.extname(filepath + file);
-            if (pathext == ".mp4") {
-                jsonstruct.cast = true;
-                jsonstruct.video = true;
-                filelist.push(jsonstruct);
-            } else if (pathext == ".mkv" || pathext == ".avi") {
-                // This is a video file, but not castable
-                jsonstruct.cast = false;
-                jsonstruct.video = true;
-                filelist.push(jsonstruct);
-            } else {
-                // For other normal files...
-                filelist.push(jsonstruct);
-            }
-        }
-    });
+      filelist.push(jsonstruct);
+    } else {
+      // Not a directory. Check if its a castable file type (mp4)
+      var pathext = path.extname(filepath + file);
+      if (pathext == ".mp4") {
+        jsonstruct.cast = true;
+        jsonstruct.video = true;
+        filelist.push(jsonstruct);
+      } else if (pathext == ".mkv" || pathext == ".avi") {
+        // This is a video file, but not castable
+        jsonstruct.cast = false;
+        jsonstruct.video = true;
+        filelist.push(jsonstruct);
+      } else {
+        // For other normal files...
+        filelist.push(jsonstruct);
+      }
+    }
+  });
 
-    return filelist;
+  return filelist;
 }
 
 module.exports = RepCast;
