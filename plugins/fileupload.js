f599cb88fe9827bc9c56e1a789745dfd1e09fcf3
Mark Repka <mark@repkam09.com>
2019-06-28 14:52:48 -0400

    Cleanup some no longer used plugins

--- plugins/fileupload.js
+++ plugins/fileupload.js
@@ -1,60 +1,42 @@
 const LifeforcePlugin = require("../utils/LifeforcePlugin.js");
 const fs = require("fs");
 
-
 class FileUploads extends LifeforcePlugin {
-    constructor(restifyserver, logger, name) {
-        super(restifyserver, logger, name);
-        this.apiMap = [
-            {
-                path: "/api/files/upload",
-                type: "post",
-                handler: handleFileUpload
-            },
-            {
-                path: "/api/imgur/upload",
-                type: "get",
-                handler: handleImgurCache
-            }
-        ];
-    }
+  constructor(restifyserver, logger, name) {
+    super(restifyserver, logger, name);
+    this.apiMap = [
+      {
+        path: "/api/files/upload",
+        type: "post",
+        handler: handleFileUpload
+      }
+    ];
+  }
 }
 
 function handleFileUpload(req, res, next) {
-    if (req.files) {
-        var data = req.files.filedata;
-        var path = data.path;
-        var newpath = this.config.logpathhidden + "uploads/" + data.name;
-	console.log("Current Path:" + path);
-	console.log("New Path:" + newpath);
-        fs.rename(path, newpath, (err) => {
-            if (err) {
-                this.log.error("Error moving file from " + path + " to " + newpath);
-            } else {
-                this.log.verbose("Moved file from " + path + " to " + newpath);
-            }
-        });
+  if (req.files) {
+    var data = req.files.filedata;
+    var path = data.path;
+    var newpath = this.config.logpathhidden + "uploads/" + data.name;
+    console.log("Current Path:" + path);
+    console.log("New Path:" + newpath);
+    fs.rename(path, newpath, err => {
+      if (err) {
+        this.log.error("Error moving file from " + path + " to " + newpath);
+      } else {
+        this.log.verbose("Moved file from " + path + " to " + newpath);
+      }
+    });
 
-        this.log.info("Got file: " + data.name);
-        this.log.verbose("Hash: " + data.hash);
-        res.send(200, {url: "https://repkam09.com/dl/uploads/" + data.name});
-    } else {
-        res.send(200, { filename: false });
-    }
+    this.log.info("Got file: " + data.name);
+    this.log.verbose("Hash: " + data.hash);
+    res.send(200, { url: "https://repkam09.com/dl/uploads/" + data.name });
+  } else {
+    res.send(200, { filename: false });
+  }
 
-    next();
-}
-
-function handleImgurCache(req, res, next) {
-    // Download and cache the image for future loads
-    if (req.params.url) {
-        var url = "http://i.imgur.com/" + req.params.url + ".jpg";
-        urlcache(url, "imgur", config.logpathhidden).then((path) => {
-            fs.createReadStream(path).pipe(res);
-        }).catch((error) => {
-            res.send(500, error);
-        });
-    }
+  next();
 }
 
 module.exports = FileUploads;
--- plugins/telegram.js
+++ plugins/telegram.js
@@ -5,54 +5,60 @@ let readySend = false;
 let readyTimer = null;
 
 class TelegramBot extends LifeforcePlugin {
-    constructor(restifyserver, logger, name) {
-        super(restifyserver, logger, name);
-        this.apiMap = [
-            {
-                path: "/api/telegram/log",
-                type: "post",
-                handler: handleTelegramLog
-            }
-        ];
+  constructor(restifyserver, logger, name) {
+    super(restifyserver, logger, name);
+    this.apiMap = [
+      {
+        path: "/api/telegram/log",
+        type: "post",
+        handler: handleTelegramLog
+      }
+    ];
 
-        const TGBot = require('node-telegram-bot-api');
-        this.bot = new TGBot(this.config.telegram.token, { polling: true });
+    const TGBot = require("node-telegram-bot-api");
+    this.bot = new TGBot(this.config.telegram.token, { polling: true });
 
-        this.bot.on('message', (msg) => {
-            const chatId = msg.chat.id;
+    this.bot.on("message", msg => {
+      const chatId = msg.chat.id;
 
-            // send a message to the chat acknowledging receipt of their message 
-            this.bot.sendMessage(chatId, "Hello! Here is the information about your chat message: \n" + JSON.stringify(msg.chat));
-        });
+      // send a message to the chat acknowledging receipt of their message
+      this.bot.sendMessage(
+        chatId,
+        "Hello! Here is the information about your chat message: \n" +
+          JSON.stringify(msg.chat)
+      );
+    });
 
-        if (this.config.telegram.logger) {
-            var messageSend = function (message) {
-                messageQueue.push(message);
-            }
+    if (this.config.telegram.logger) {
+      var messageSend = function(message) {
+        messageQueue.push(message);
+      };
 
-            logger.registerCallback(messageSend.bind(this));
+      logger.registerCallback(messageSend.bind(this));
 
-            readyTimer = setInterval(() => {
-                if (messageQueue.length > 0) {
-                    this.bot.sendMessage(this.config.telegram.chatid, messageQueue.join("\n"));
-                    messageQueue = [];
-                }
-            }, 10000);
+      readyTimer = setInterval(() => {
+        if (messageQueue.length > 0) {
+          this.bot.sendMessage(
+            this.config.telegram.chatid,
+            messageQueue.join("\n")
+          );
+          messageQueue = [];
         }
+      }, 10000);
     }
+  }
 }
 
 function handleTelegramLog(req, res, next) {
-    if (req.body) {
-        var payload = req.body + "";
+  if (req.body) {
+    var payload = req.body + "";
 
-        // send a message to the chat acknowledging receipt of their message 
-        this.bot.sendMessage(this.config.telegram.chatid, payload);
-        res.send(200, { error: false });
-    } else {
-        res.send(400);
-    }
+    // send a message to the chat acknowledging receipt of their message
+    this.bot.sendMessage(this.config.telegram.chatid, payload);
+    res.send(200, { error: false });
+  } else {
+    res.send(400);
+  }
 }
 
 module.exports = TelegramBot;
-
