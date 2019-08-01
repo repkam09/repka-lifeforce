dcc0d7589e5766a11a52820c8241b5dead88187a
repkam09 <mark@repkam09.com>
2019-07-31 21:41:09 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+sudo service repka-lifeforce stop
 git pull
-pm2 restart lifeforce
+npm install 
+sudo service repka-lifeforce start
+sudo service repka-lifeforce status
--- restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo service repka-lifeforce stop
-git pull
-npm install 
-sudo service repka-lifeforce start
-sudo service repka-lifeforce status
