fae72dc95b57d35a9040823b69df3d43bc9f267b
Mark Repka <mark@repkam09.com>
2020-02-06 12:52:51 -0500

    Update RS plugin

--- plugins/runescape.js
+++ plugins/runescape.js
@@ -1,5 +1,7 @@
 const LifeforcePlugin = require("../utils/LifeforcePlugin.js");
 
+const PlayerMap = new Map();
+
 class RuneScape extends LifeforcePlugin {
   constructor(restifyserver, logger, name) {
     super(restifyserver, logger, name);
@@ -16,11 +18,31 @@ class RuneScape extends LifeforcePlugin {
 
 function handleRsMetericsPlayerInfo(req, res, next) {
   if (req.params.username) {
-    let url =
-      "https://apps.runescape.com/runemetrics/profile/profile?user=" +
-      req.params.username +
-      "&activities=20";
-    this.request.get(url).pipe(res);
+
+    // Check if we have data already in the cache
+    if (PlayerMap.has(req.params.username)) {
+      this.log.info("Responding from player data cache");
+      res.send(200, PlayerMap.get(req.params.username));
+      return;
+    }
+
+
+    let url = "https://apps.runescape.com/runemetrics/profile/profile?user=" + req.params.username + "&activities=20";
+    this.request.get(url, (err, response, body) => {
+      if (err) {
+        res.send(500, err.message);
+        return;
+      }
+
+      if (response.statusCode === 200) {
+        this.log.info("Updating player data cache for " + req.params.username);
+        PlayerMap.set(req.params.username, body);
+        res.send(200, body);
+        return
+      }
+
+      res.send(response.statusCode, body);
+    });
   } else {
     res.send(400);
   }
