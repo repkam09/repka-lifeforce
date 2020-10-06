9049fc6f7f4ffce42d5f352353d4917944343f68
Mark Repka <mark@repkam09.com>
2020-10-06 19:27:42 -0400

    Add dates back to repcast response

--- plugins/repcast.js
+++ plugins/repcast.js
@@ -1,5 +1,4 @@
 const LifeforcePlugin = require("../utils/LifeforcePlugin.js");
-const fs = require("fs");
 const Transmission = require('transmission');
 
 String.prototype.replaceAll = function (search, replacement) {
@@ -41,33 +40,6 @@ function handleRepcastTorAdd(req, res, next) {
         res.send(500, err.message);
 
     }
-}
-
-function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-
-    var interval = Math.floor(seconds / 31536000);
-
-    if (interval > 1) {
-        return interval + " years";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes";
-    }
-    return Math.floor(seconds) + " seconds";
 }
 
 module.exports = RepCast;
--- plugins/repcastnas.js
+++ plugins/repcastnas.js
@@ -72,6 +72,32 @@ function handleResetCache(req, res, next) {
     res.send(200, "OK");
 }
 
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
 
 
 function handleRepcastDirGet(req, res, next) {
@@ -161,6 +187,12 @@ function dirlist(filepath) {
             jsonstruct.original = file;
             jsonstruct.mimetype = mimetype.lookup(ext);
             jsonstruct.filetype = ext;
+
+            try {
+                jsonstruct.date = timeSince(stats.mtimeMs);
+            } catch (err) {
+                // Ignore that we cant get a proper date
+            }
         }
 
         filelist.push(jsonstruct);
