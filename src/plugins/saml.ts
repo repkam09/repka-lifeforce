9dd9236f0fe74ab86626c7aa69e76b7a007ec5fb
Mark Repka <mark.repka@yum.com>
2025-09-10 11:15:29 -0400

    saml debug

--- src/plugins/saml.ts
+++ src/plugins/saml.ts
@@ -7,6 +7,7 @@ import {
   LifeforePluginConfiguration,
 } from "../utils/LifeforcePlugin";
 import { Logger } from "../utils/logger";
+import { Config } from "../utils/config";
 
 export class SSODebug extends LifeforcePlugin {
   constructor(input: LifeforePluginConfiguration) {
@@ -32,6 +33,15 @@ export class SSODebug extends LifeforcePlugin {
         auth: false,
       },
     ]);
+
+    // Configure a default mock SAML provider with mock data
+    this.configureSamlStrategy({
+      entryPoint: "https://mocksaml.com/api/saml/sso",
+      issuer: "lifeforce",
+      audience: "lifeforce",
+      cert: "MIIC4jCCAcoCCQC33wnybT5QZDANBgkqhkiG9w0BAQsFADAyMQswCQYDVQQGEwJVSzEPMA0GA1UECgwGQm94eUhRMRIwEAYDVQQDDAlNb2NrIFNBTUwwIBcNMjIwMjI4MjE0NjM4WhgPMzAyMTA3MDEyMTQ2MzhaMDIxCzAJBgNVBAYTAlVLMQ8wDQYDVQQKDAZCb3h5SFExEjAQBgNVBAMMCU1vY2sgU0FNTDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALGfYettMsct1T6tVUwTudNJH5Pnb9GGnkXi9Zw/e6x45DD0RuRONbFlJ2T4RjAE/uG+AjXxXQ8o2SZfb9+GgmCHuTJFNgHoZ1nFVXCmb/Hg8Hpd4vOAGXndixaReOiq3EH5XvpMjMkJ3+8+9VYMzMZOjkgQtAqO36eAFFfNKX7dTj3VpwLkvz6/KFCq8OAwY+AUi4eZm5J57D31GzjHwfjH9WTeX0MyndmnNB1qV75qQR3b2/W5sGHRv+9AarggJkF+ptUkXoLtVA51wcfYm6hILptpde5FQC8RWY1YrswBWAEZNfyrR4JeSweElNHg4NVOs4TwGjOPwWGqzTfgTlECAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAAYRlYflSXAWoZpFfwNiCQVE5d9zZ0DPzNdWhAybXcTyMf0z5mDf6FWBW5Gyoi9u3EMEDnzLcJNkwJAAc39Apa4I2/tml+Jy29dk8bTyX6m93ngmCgdLh5Za4khuU3AM3L63g7VexCuO7kwkjh/+LqdcIXsVGO6XDfu2QOs1Xpe9zIzLpwm/RNYeXUjbSj5ce/jekpAw7qyVVL4xOyh8AtUW1ek3wIw1MJvEgEPt0d16oshWJpoS1OT8Lr/22SvYEo3EmSGdTVGgk3x3s+A0qWAqTcyjr7Q4s/GKYRFfomGwz0TZ4Iw1ZN99Mm0eo2USlSRTVl7QHRTuiuSThHpLKQQ==",
+      callbackUrl: Config.LIFEFORCE_PUBLIC_URL + "/api/sso/saml/callback",
+    });
   }
 
   public async init(): Promise<void> {
@@ -40,68 +50,44 @@ export class SSODebug extends LifeforcePlugin {
 
   private handlePostSAMLCallback(ctx: Context, next: Next) {
     const body: Record<string, any> = ctx.request.body ?? {};
-
-    passport.authenticate(
+    return passport.authenticate(
       "saml",
       (err: Error, user: unknown, info: unknown) => {
         Logger.info("SAML Authentication Callback Invoked");
         if (err) {
-          const response = {
-            url: ctx.request.url,
-            params: ctx.request.query,
-            headers: ctx.request.headers,
-            keys: Object.keys(body),
-            SAMLResponse: body.SAMLResponse ? "Received" : "Missing",
-            RelayState: body.RelayState ? "Received" : "Missing",
-          };
-
           ctx.status = 200;
           ctx.body = {
             error: "SAML Authentication Error",
             details: err,
-            debug: response,
           };
           return next();
         }
 
         if (!user) {
-          const response = {
-            url: ctx.request.url,
-            params: ctx.request.query,
-            headers: ctx.request.headers,
-            keys: Object.keys(body),
-            SAMLResponse: body.SAMLResponse ? "Received" : "Missing",
-            RelayState: body.RelayState ? "Received" : "Missing",
-          };
-
           ctx.status = 200;
           ctx.body = {
             error: "SAML Authentication Failed",
             details: info,
-            debug: response,
           };
           return next();
         }
 
         ctx.status = 200;
-        ctx.body = { message: "SAML Authentication Successful", user: user };
+        ctx.body = {
+          error: null,
+          details: {
+            params: ctx.request.query,
+            headers: ctx.request.headers,
+            keys: Object.keys(body),
+            SAMLResponse: body.SAMLResponse ? "Received" : "Missing",
+            RelayState: body.RelayState ? "Received" : "Missing",
+          },
+          user: user,
+          relayState: body.RelayState || null,
+        };
         return next();
       }
-    );
-
-    const response = {
-      url: ctx.request.url,
-      params: ctx.request.query,
-      headers: ctx.request.headers,
-      keys: Object.keys(body),
-      SAMLResponse: body.SAMLResponse ? "Received" : "Missing",
-      RelayState: body.RelayState ? "Received" : "Missing",
-    };
-
-    ctx.status = 200;
-    ctx.body = response;
-
-    return next();
+    )(ctx, next);
   }
 
   private handlePostSAMLConfigure(ctx: Context, next: Next) {
@@ -113,24 +99,33 @@ export class SSODebug extends LifeforcePlugin {
       return next();
     }
 
-    passport.use(
-      "saml",
-      new SamlStrategy(
-        body,
-        (profile: any, callback: (error: Error | null, user?: any) => void) => {
-          Logger.info("SAML Strategy Callback Invoked");
-          return callback(null, profile);
-        }
-      )
-    );
+    Logger.info("Configuring SAML Strategy with provided settings");
+    this.configureSamlStrategy(body);
 
     ctx.status = 200;
     ctx.body = { message: "SAML Configuration Updated", config: body };
     return next();
   }
 
+  private configureSamlStrategy(config: SamlConfig) {
+    passport.use(
+      "saml",
+      new SamlStrategy(
+        config,
+        (profile: any, callback: (error: Error | null, user?: any) => void) => {
+          Logger.info("SAML Strategy Callback Invoked");
+          return callback(null, profile);
+        }
+      )
+    );
+  }
+
   private handleGetSAMLLogin(ctx: Context, next: Next) {
     console.log("SAML Login Handler Invoked");
-    return passport.authenticate("saml")(ctx, next);
+    return passport.authenticate("saml", {
+      additionalParams: {
+        RelayState: ctx.query.RelayState || "",
+      },
+    })(ctx, next);
   }
 }
